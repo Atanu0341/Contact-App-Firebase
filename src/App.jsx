57f4fa0c80e75431be59522f276badceb63cd1c3
Atanu Majumder <atanumajumder2004@gmail.com>
2024-01-09 20:56:28 +0530

    Add toast notifications and search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import { db } from "./config/firebase";
 import ContactCard from "./components/ContactCard";
 import AddAndUpdateContact from "./components/AddAndUpdateContact";
 import useDisclose from "./hooks/useDisclose";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import NotFoundContact from "./components/NotFoundContact";
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
@@ -36,6 +39,27 @@ const App = () => {
     getContacts();
   }, []);
 
+  const filterContacts = (e) => {
+    const value = e.target.value;
+
+    const contactsRef = collection(db, "contacts");
+
+    onSnapshot(contactsRef, (snapshot) => {
+      const contactLists = snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+
+      const filteredContacts = contactLists.filter((contact) =>
+        contact.name.toLowerCase().includes(value.toLowerCase())
+      );
+      setContacts(filteredContacts);
+      return filteredContacts;
+    });
+  };
+
   return (
     <>
       <div className="max-w-[370px] mx-auto px-4">
@@ -45,6 +69,7 @@ const App = () => {
             <FiSearch className="text-white absolute text-3xl ml-1" />
             <input
               type="text"
+              onChange={filterContacts}
               className="bg-transparent border text-white pl-9 border-white rounded-md h-10 flex-grow"
             />
           </div>
@@ -54,12 +79,17 @@ const App = () => {
           />
         </div>
         <div className="mt-4 gap-3 flex flex-col">
-          {contacts.map((contact) => (
-            <ContactCard key={contact.id} contact={contact} />
-          ))}
+          {contacts.length <= 0 ? (
+            <NotFoundContact />
+          ) : (
+            contacts.map((contact) => (
+              <ContactCard key={contact.id} contact={contact} />
+            ))
+          )}
         </div>
       </div>
       <AddAndUpdateContact onClose={onClose} isOpen={isOpen} />
+      <ToastContainer position="bottom-center" />
     </>
   );
 };

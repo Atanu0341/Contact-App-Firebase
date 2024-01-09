b2c76cce54bef1319d0d28ab38a2a8c40481b46d
Atanu Majumder <atanumajumder2004@gmail.com>
2024-01-09 20:12:12 +0530

    Add delete functionality and update contact form

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -2,25 +2,44 @@ import React from "react";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { IoMdTrash } from "react-icons/io";
 import { RiEditCircleLine } from "react-icons/ri";
+import { db } from "../config/firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import AddAndUpdateContact from "./AddAndUpdateContact";
+import useDisclose from "../hooks/useDisclose";
 
 const ContactCard = ({ contact }) => {
+  const { isOpen, onClose, onOpen } = useDisclose();
+
+  const deleteContact = async (id) => {
+    try {
+      await deleteDoc(doc(db, "contacts", id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div
-      key={contact.id}
-      className="bg-yellow p-2 rounded-lg flex items-center justify-between"
-    >
-      <div className="flex gap-1">
-        <HiOutlineUserCircle className="text-orange text-4xl" />
-        <div className="">
-          <h2 className="font-medium">{contact.Name}</h2>
-          <p className="text-sm">{contact.Email}</p>
+    <>
+      <div
+        key={contact.id}
+        className="bg-yellow p-2 rounded-lg flex items-center justify-between"
+      >
+        <div className="flex gap-1">
+          <HiOutlineUserCircle className="text-orange text-4xl" />
+          <div className="">
+            <h2 className="font-medium">{contact.name}</h2>
+            <p className="text-sm">{contact.email}</p>
+          </div>
+        </div>
+        <div className="flex text-3xl">
+          <RiEditCircleLine onClick={onOpen} className="cursor-pointer"/>
+          <IoMdTrash
+            onClick={() => deleteContact(contact.id)}
+            className="text-orange cursor-pointer"
+          />
         </div>
       </div>
-      <div className="flex text-3xl">
-        <RiEditCircleLine />
-        <IoMdTrash className="text-orange" />
-      </div>
-    </div>
+      <AddAndUpdateContact contact={contact} isUpdate isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
 

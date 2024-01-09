9520ed771225c0b9f0d7d0f5a365204ebcd00d11
Atanu Majumder <atanumajumder2004@gmail.com>
2024-01-09 20:11:23 +0530

    Refactor Modal component to use createPortal and add close button

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { createPortal } from "react-dom";
+import { AiOutlineClose } from "react-icons/ai";
 
-const Modal = ({onClose, isOpen, children}) => {
-  return <>{isOpen && <div>Modal</div>}</>;
+const Modal = ({ onClose, isOpen, children }) => {
+  return createPortal(
+    <>
+      {isOpen && (
+        <>
+          <div className="m-auto relative z-50 p-4 min-h-[200px] max-w-[80%] bg-white">
+            <div className="flex justify-end ">
+              <AiOutlineClose
+                onClick={onClose}
+                className="text-2xl cursor-pointer"
+              />
+            </div>
+            {children }
+          </div>
+          <div
+            onClick={onClose}
+            className="backdrop-blur z-40 h-screen w-screen absolute top-0"
+          />
+        </>
+      )}
+    </>,
+    document.getElementById("modal-root")
+  );
 };
 
 export default Modal;

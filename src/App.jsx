c2abbf76d9e71304ed9de85d3a35d1b8c2f69711
Atanu Majumder <atanumajumder2004@gmail.com>
2024-01-09 15:06:24 +0530

    Refactor App component and add new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
+import ContactCard from "./components/ContactCard";
+import Modal from "./components/Modal";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  const [isOpen, setOpen] = useState(false);
+
+  const onOpen = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setClose(false);
+  };
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactsRef = collection(db, "contacts");
+        const contactsSnapshot = await getDocs(contactsRef);
+        const contactLists = contactsSnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setContacts(contactLists);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getContacts();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="max-w-[370px] mx-auto px-4">
+        <Navbar />
+        <div className="flex gap-2">
+          <div className="flex items-center relative flex-grow">
+            <FiSearch className="text-white absolute text-3xl ml-1" />
+            <input
+              type="text"
+              className="bg-transparent border text-white pl-9 border-white rounded-md h-10 flex-grow"
+            />
+          </div>
+          <AiFillPlusCircle onClick={onOpen} className="text-5xl text-white cursor-pointer" />
+        </div>
+        <div className="mt-4 gap-3 flex flex-col">
+          {contacts.map((contact) => (
+            <ContactCard key={contact.id} contact={contact} />
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        Hi
+      </Modal>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
